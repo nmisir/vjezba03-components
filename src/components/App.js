704e6c4e4698957d1293ba37dda10e9f3e6d876f
Nikola Misir <nikola.misir89@gmail.com>
2021-12-13 10:54:03 +0100

    Add app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import HelloClass from "./HelloClass";
+import HelloFunction from "./HelloFunction";
+
+
+function App() {
+  return (
+    <div>
+      <HelloClass />
+      <HelloFunction />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloFunction.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HelloFunction(){
+    return <h2>Hello!</h2>
+  }
